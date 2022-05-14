8d67c11fb3f97fdcf64e09af35353c326d5ac155
sivakrishnareddy.p <sivakrishnareddy.p@presidio.com>
2022-05-15 00:44:04 +0530

    First Commit - React TypeScript application boilerplate with context api sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import NewToDos from './components/NewToDos/NewToDos';
+import Todo from './components/Todo/Todo';
+import TodoContextProvider from './context/TodoContenxt';
+import Todos from './models/Todos';
 
 function App() {
+
+  // let data = [new Todos("item 1"), new Todos('item 2'), new Todos('item 3')]
+  // let [todoList, settodoList] = useState<Todos[]>([]);
+
+  // const addTodoitem = (itemName: string) =>{
+  //   console.log("EMITTED s");
+  //     // todoList = [...todoList, new Todos(itemName)];
+  //     settodoList((prevtodoList)=>{
+  //       return prevtodoList.concat(new Todos(itemName))
+  //     })
+  // }
+
+  // const removeTodoItemHandler = (todoId: string) => {
+  //   settodoList(prevtodoList => {
+  //     return prevtodoList.filter((data) => data.id !==todoId )
+  //   })
+  // }
+  // //Clean up
+  // useEffect(()=>{
+  //   return () => {
+  //     settodoList([])
+  //   }
+  // },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoContextProvider>
+        <NewToDos/>
+        <Todo />
+        </TodoContextProvider>
+        {/* <NewToDos addItem={addTodoitem} ></NewToDos>
+        <Todo items={todoList} removeTodoItem={(todoId: string) => {removeTodoItemHandler(todoId)}}/> */}
     </div>
   );
 }
